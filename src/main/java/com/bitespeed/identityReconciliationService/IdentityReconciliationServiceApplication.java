56b1dfdcbdab8223a88a883e4034b2f6f2c69a76
agharshu17 <harshitaa2000@gmail.com>
2024-04-23 00:11:43 +0530

    repository setup

---
+++ src/main/java/com/bitespeed/identityReconciliationService/IdentityReconciliationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bitespeed.identityReconciliationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityReconciliationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityReconciliationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bitespeed/identityReconciliationService/IdentityReconciliationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitespeed.identityReconciliationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityReconciliationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
